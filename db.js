8fcf45de001ca3b0bdab9746e9d9d070e9fa11cf
Haidezai <haidepzai269@gmail.com>
2025-07-14 15:26:39 +0700

    Cập nhật lại file db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-console.log('🔗 DATABASE_URL:', process.env.DATABASE_URL); // 👈 Thêm dòng này
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+  host: 'db.ggjadhmvoflzsgokfozj.supabase.co', // 🟢 Buộc dùng hostname IPv4
+  port: 5432,
 });
+
+console.log('🔗 DATABASE_URL:', process.env.DATABASE_URL);
 
 module.exports = pool;
