0b5c34ccdebe4600f92546afcd9493aa304f31d7
Haidezai <haidepzai269@gmail.com>
2025-07-14 15:06:47 +0700

    Cập nhật lại file db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 // Du-an-BE/db.js
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Bắt buộc khi dùng Supabase / Render
+  },
 });
-
-
-  
 
 module.exports = pool;
