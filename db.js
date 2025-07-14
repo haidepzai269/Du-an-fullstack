47766bca3a3b2ef68b1f7139adcbf771ccb2fcee
Haidezai <haidepzai269@gmail.com>
2025-07-14 15:18:30 +0700

    Log DB URL for debug

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-// Du-an-BE/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
+
+console.log('🔗 DATABASE_URL:', process.env.DATABASE_URL); // 👈 Thêm dòng này
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Bắt buộc khi dùng Supabase / Render
+    rejectUnauthorized: false,
   },
 });
 
