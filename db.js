8748f2dabc8ac0f7c9e4fad6908822bfbe50bc86
Haidezai <haidepzai269@gmail.com>
2025-07-14 16:25:25 +0700

    Cập nhật lại file db.js

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: 'db.ggjadhmvoflzsgokfozj.supabase.co',
+  host: 'db.ggjadhmvoflzsgokfozj.supabase.co',     // ✅ IPv4 host
   port: 5432,
   user: 'postgres',
   password: 'haidepzai269',
   database: 'postgres',
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
-console.log('✅ Đang kết nối tới Supabase...');
+console.log('✅ Đã cấu hình PostgreSQL thành công');
 
 module.exports = pool;
